2dd20d896f758a3a74058dea76a0e2a877d280a1
ArtOfLuxis <159471757+ArtOfLuxis@users.noreply.github.com>
2026-09-18 23:18:49 +0500

    Revert "first attempt at integrating eslint"

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
-import checker from 'vite-plugin-checker';
 
 export default defineConfig({
     build: {
-        assetsInlineLimit: 4096,
-        minify: "esbuild",
+        assetsInlineLimit: 100000000, //100mb
+        minify: "oxc",
         rollupOptions: {
             input: 'src/main.js',
             preserveEntrySignatures: "allow-extension",
@@ -13,13 +12,5 @@ export default defineConfig({
                 format: 'esm',
             }
         }
-    },
-    plugins: [
-        checker({
-            eslint: {
-                useFlatConfig: true,
-                lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
-            },
-        }),
-    ],
+    }
 });
